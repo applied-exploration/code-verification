a9c2aa5781d91e85e714fe516c100ee43d7146f3
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-06-01 17:15:50 +0200

    feat: Continous dataset vs precalculated.

--- data/dataset.py
+++ data/dataset.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import torch as t
 from torch.nn.utils.rnn import pad_sequence
@@ -7,30 +6,46 @@ from transformers import pipeline
 
 from utils.text_process import strip_comments
 
+
 class SourceCodeDataset(Dataset):
-    def __init__(self, all_code: pd.DataFrame):
+    def __init__(self, all_code: pd.DataFrame, all_features: dict):
         self.all_code = all_code
-        self.extractor = pipeline("feature-extraction", framework='pt')
-        
+        self.all_features = all_features
+
     def __len__(self):
         return len(self.all_code)
 
     def __getitem__(self, idx):
-        source_code = preprocess_source(self.all_code.iloc[idx]['content'])
-        features = get_features(source_code)
-        
+        source_code = preprocess_source(self.all_code.iloc[idx]["content"])
+        features = self.all_features[idx]
+
         return source_code, features
 
-def get_features(original:str, extractor:pipeline)->t.Tensor:
+
+class SourceCodeDatasetRuntime(Dataset):
+    def __init__(self, all_code: pd.DataFrame):
+        self.all_code = all_code
+        self.extractor = pipeline("feature-extraction", framework="pt")
+
+    def __len__(self):
+        return len(self.all_code)
+
+    def __getitem__(self, idx):
+        source_code = preprocess_source(self.all_code.iloc[idx]["content"])
+        features = get_features(source_code)
+
+        return source_code, features
+
+
+def get_features(original: str, extractor) -> t.Tensor:
     feature_tensors = [t.tensor(extractor(line)[0]) for line in original]
     features = pad_sequence(feature_tensors, batch_first=False)
     return features
-    
-def preprocess_source(original:str)->str:
+
+
+def preprocess_source(original: str) -> list[str]:
     split_by_line = original.split("\n")
     stripped_lines = [strip_comments(line) for line in split_by_line]
-    no_empty_lines = [line for line in stripped_lines if line != '']
-    
+    no_empty_lines = [line for line in stripped_lines if line != ""]
+
     return no_empty_lines
-    
-    
