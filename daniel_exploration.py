de3d19c7275ac83ec15dd14ade78f060678dc31c
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-06-01 14:53:13 +0200

    feat: Added text processing.

--- daniel_exploration.py
+++ daniel_exploration.py
@@ -1,14 +1,25 @@
 #%%
 import json
 import pandas as pd
+import torch as t
+from transformers import pipeline
+import re
+from utils.text_process import remove_comments_and_docstrings, stripComments
 
 df = pd.read_json('data/derived/python-pytorch.json')
 
-from transformers import pipeline
 
-extractor = pipeline("feature-extraction")
+list_of_objects = df.iloc[0]['content'].split("\n")
+stripCommentsstripped_lines = [stripComments(line) for line in list_of_objects]
+no_empty_lines = [line for line in stripped_lines if line != '']
 
-print(extractor("Why are there so many cars in Berlin?"))
+longest_line = len(max(list_of_objects, key=len))
 
-df['features'] = extractor(df['content'])
-df.head()
+
+extractor = pipeline("feature-extraction", framework='pt')
+features = t.tensor([extractor(line) for line in list_of_objects])
+
+print("")
+
+
+
