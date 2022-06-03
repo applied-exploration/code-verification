fa0a326f977c61a798db02bb683a1d31cf6cdb41
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-06-03 17:06:31 +0200

    fix(VectorSearch): trying to get faiss to not to die

--- run_build_vector_db_faiss.py
+++ run_build_vector_db_faiss.py
@@ -6,7 +6,8 @@ from scipy import spatial
 vectors_orig = t.load("data/temporary/features_2.pt")[0][0].numpy()
 print(1)
 index = faiss.index_factory(vectors_orig.shape[1], "Flat", faiss.METRIC_INNER_PRODUCT)
-vectors = faiss.normalize_L2(vectors_orig)
+# vectors = faiss.normalize_L2(vectors_orig)
+vectors = vectors_orig
 index.add(vectors)
 
 q = vectors_orig[:1]
@@ -15,8 +16,9 @@ distance, index = index.search(faiss.normalize_L2(vectors_orig[:3]), 5)
 print(distance, index)
 # result = 1 - spatial.distance.cosine(dataSetI, dataSetII)
 
-ncentroids = 100
+ncentroids = 10
 niter = 20
 verbose = True
 kmeans = faiss.Kmeans(vectors_orig.shape[1], ncentroids, niter=niter, verbose=verbose)
 kmeans.train(vectors)
+print(kmeans.centroids)
