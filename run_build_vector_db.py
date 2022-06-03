d6e9e77941a38419d20d5d8eaf547eddf7a53296
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-06-03 16:20:21 +0200

    feat(Search): added faiss implementation

--- run_build_vector_db.py
+++ run_build_vector_db.py
@@ -3,13 +3,11 @@ from annoy import AnnoyIndex
 
 vectors = t.load("data/temporary/features_2.pt")[0][0].numpy()
 print(1)
-t = AnnoyIndex(vectors.shape[1], 'angular')
-t.add_item()
-index.add(vectors)  # add vectors to the index
-t.build(10) # 10 trees
+db = AnnoyIndex(vectors.shape[1], "angular")
+for i, vector in enumerate(vectors):
+    db.add_item(i, vector)
+db.build(10)  # 10 trees
 
 
-k = 4  # we want to see 4 nearest neighbors
-D, indicies = index.search(vectors[:1], k)  # sanity check
-print(I)
-print(D)
+result = db.get_nns_by_vector(vectors[1], 10, include_distances=True)
+print(result)
