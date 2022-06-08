af8e75968eb8b083986097600f90f31447216c62
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-06-08 14:28:30 +0200

    feature(Dataset): load TSSB-3M dataset

---
+++ extractors/pipeline.py
@@ -0,0 +1,26 @@
+from typing import Dict, List
+from transformers.pipelines.feature_extraction import FeatureExtractionPipeline
+import torch as t
+
+
+class MyFeatureExtractionPipeline(FeatureExtractionPipeline):
+    def preprocess(self, inputs, truncation=None) -> Dict[str, t.Tensor]:
+        return_tensors = self.framework
+
+        model_inputs = self.tokenizer(inputs, return_tensors=return_tensors)
+
+        if hasattr(self, "input_tokenized_length") == False:
+            self.input_tokenized_length = []
+
+        self.input_tokenized_length.append(model_inputs["input_ids"].shape[1])
+
+        return model_inputs
+
+    def postprocess(self, model_outputs):
+        # [0] is the first available tensor, logits or last_hidden_state.
+        outputs = model_outputs[0].tolist()
+
+        for i, output in enumerate(outputs):
+            outputs[i] = output[: self.input_tokenized_length[i]]
+
+        return outputs
---
+++ run_extract_embeddings_diff.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from transformers.pipelines.base import Pipeline
+from config import PreprocessConfig, preprocess_config
+from data.dataset import RawDataset
+from transformers.pipelines import pipeline
+from torch.nn.utils.rnn import pad_sequence
+from tqdm import tqdm
+from extractors.pipeline import MyFeatureExtractionPipeline
+import torch as t
+
+
+def extract_embeddings_from_pipeline(pipe: Pipeline, dataset: RawDataset) -> t.Tensor:
+    # pipeline padding adds a unique padding token, that also gets passed into inference, hence we get a feature tensor for the padding tokens aswell.
+    embeddings = [
+        t.mean(t.tensor(out)[0], dim=0, keepdim=False)
+        for out in tqdm(
+            pipe(dataset, batch_size=config.batch_size, padding=True),
+            total=len(df),
+        )
+    ]
+
+    print("| Restore original order for embeddings...")
+    embeddings: t.Tensor = pad_sequence(embeddings, batch_first=True)
+    embeddings = embeddings[dataset.get_sort_indices()]
+    return embeddings
+
+
+def run_extract_embeddings(config: PreprocessConfig):
+    device = 0 if t.cuda.is_available() else -1
+
+    if config.force_cpu:
+        print("| Setting device to cpu...")
+        device = -1
+
+    print("| Running preprocessing...")
+    df = pd.read_json(
+        f"data/original/TSSB-3M/file-0.jsonl.gz", lines=True, compression="gzip"
+    )[: config.dataset_size]
+
+    print("| Setting up pipeline...")
+    pipe = pipeline(
+        "feature-extraction",
+        framework="pt",
+        model="microsoft/codebert-base",
+        device=device,
+        pipeline_class=MyFeatureExtractionPipeline,
+    )
+
+    print("| Converting original source to embeddings...")
+    before_dataset = RawDataset(df, "before")
+    before_embeddings = extract_embeddings_from_pipeline(pipe, before_dataset)
+
+    after_dataset = RawDataset(df, "after")
+    after_embeddings = extract_embeddings_from_pipeline(pipe, before_dataset)
+
+    diff_embeddings = after - before
+
+    print("| Saving embeddings to disk...")
+    t.save(diff_embeddings, "data/derived/embeddings_flat.pt")
+
+    print("✅ Feature preprocessing done...")
+
+
+if __name__ == "__main__":
+    run_extract_embeddings(preprocess_config)
