5b96b40aa5eb931da099f185c7a7ce6eb653818e
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-06-08 15:57:42 +0200

    chore: Added printlines.

--- extractors/negative_cases.py
+++ extractors/negative_cases.py
@@ -1,18 +1,24 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 
 def add_negative_cases(df: pd.DataFrame) -> pd.DataFrame:
-
+    split_ratio = 0.5
+    print(f"| Splitting dataset by {split_ratio}")
     positive, negative = train_test_split(
-        df, test_size=0.50, random_state=1, shuffle=True
+        df, test_size=split_ratio, random_state=1, shuffle=True
     )
 
+    print(f"| Copying after to before on negative rows...")
     negative["before"] = negative["after"]
-    negative["label"] = -1.0
 
+    print(f"| Adding labels to rows...")
+    negative["label"] = -1.0
     positive["label"] = 1.0
 
+    print(f"| Combining and shuffling dataframe...")
     df = pd.concat([positive, negative])
 
+    df = df.iloc[np.random.permutation(len(df))]
     return df
