8823cba3a1ad4697c5cae1cc36cb0ffeda0c6d7c
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-06-13 14:22:25 +0200

    fix(Classify): use config values

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class ClassifyConfig:
 classify_config = ClassifyConfig(
     dataset_size=-1,
     force_cpu=True,
-    batch_size=64,
+    batch_size=32,
     grad_accum_step=1,
     test_split_ratio=0.1,
     val_split_ratio=0.1,
